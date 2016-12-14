9823733380d20a922be1e69b038307d2cab807f6
renchen <renchenwork@163.com>
2016-12-14 13:39:27 +0800

    init

---
+++ src/scripts/page/about.js
@@ -0,0 +1,7 @@
+//引入css
+require("../../styles/lib/reset.css");
+require("../../styles/common/global.css");
+require("../../styles/common/grid.css");
+require("../../styles/common/common.less");
+
+$('.g-bd').append('hehe');
